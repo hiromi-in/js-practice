f3aa56a15790b9f86a24f38182d6c8798cde6fe2
hiromi-in <piromi.ishikawa@gmail.com>
2022-05-31 19:37:10 -0500

    Add 5-02

---
+++ practice/5-02_location/5-02_location.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const lang = document.querySelector('html').lang;
+
+if(lang==='ja'){
+    document.querySelector('option[value="5-02_locationJP.html"]').selected = true;
+} else if (lang === 'en'){
+    document.querySelector('option[value="5-02_locationEN.html"]').selected = true;
+} else if (lang === 'zh'){
+    document.querySelector('option[value="5-02_locationZH.html"]').selected = true;
+}
+
+document.getElementById('form').select.onchange = function(){
+    location.href = document.getElementById('form').select.value;
+}
